76b0da86ab0894898e38506d186d561ba1ed2548
shzgong <shzgong@gmail.com>
2020-11-09 12:40:11 +0800

    organize the spider of the meditation

--- spider2-冥想材料/spider2-冥想材料.py
+++ spider2-冥想材料/spider2-冥想材料.py
@@ -17,8 +17,8 @@ import time
 import lxml
 
 def get_meditation():
-    base_url = 'http://live.soundstrue.com/journeys/mindfulness-daily-free-gift/'
     host = 'live.soundstrue.com'
+    base_url = 'http://live.soundstrue.com/journeys/mindfulness-daily-free-gift/'
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
     # 直接手动复制登录之后的cookie
     cookie_str = 'live_access=%7B%22user%22%3A%221304309%22%7D; _gcl_au=1.1.77243089.1554971765; _ga=GA1.2.846266131.1554971765; _fbp=fb.1.1554971766135.1434384152; lc_sso8339831=1554971766257; __btr_em=c2h6Z29uZ0BnbWFpbC5jb20%3D; tid_7rurxl00clkk6pq1fwqc6j6yxh9mzs9t197jgtefvpplk7hy65=3.RM0.AzirZA.A-Ff.YL8N..uedn.b..s.eko.a.VT7bdQ.VT7ifQ.Cb9-gQ; mindfulness-daily-free-gift=1'
@@ -26,7 +26,7 @@ def get_meditation():
     for line in cookie_str.split(';'):
         name, value = line.strip().split('=')
         cookies[name] = value
-    headers= {'user-Agent': user_agent,'host': host}
+    headers = {'user-Agent': user_agent, 'host': host}
     html = requests.get(base_url, headers=headers, cookies=cookies)
     soup = BeautifulSoup(html.text, features="lxml")
     subsubject = soup.find_all(attrs={"class":"sub-category-head"})
@@ -43,7 +43,7 @@ def get_meditation():
 
     count = 1
     for i in range(len(subject_name)):
-        subject_path = './meditation/'+ str(i+1)+'_'+ subject_name[i].replace(' ', '_') + '/'
+        subject_path = './meditation/' + str(i+1)+'_' + subject_name[i].replace(' ', '_') + '/'
         if not os.path.exists(subject_path):
             os.mkdir(subject_path)
         for j in range(session_length[i]):
