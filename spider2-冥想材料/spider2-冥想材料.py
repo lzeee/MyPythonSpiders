6112797ac67aa6c00c65f7bd0226225ffc429ccd
shzgong <shzgong@gmail.com>
2020-11-09 16:45:28 +0800

    organize the spider

--- spider2-冥想材料/spider2-冥想材料.py
+++ spider2-冥想材料/spider2-冥想材料.py
@@ -21,6 +21,7 @@ def get_meditation():
     base_url = 'http://live.soundstrue.com/journeys/mindfulness-daily-free-gift/'
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
     # 直接手动复制登录之后的cookie
+    # 用来伪装自己是真实的用户
     cookie_str = 'live_access=%7B%22user%22%3A%221304309%22%7D; _gcl_au=1.1.77243089.1554971765; _ga=GA1.2.846266131.1554971765; _fbp=fb.1.1554971766135.1434384152; lc_sso8339831=1554971766257; __btr_em=c2h6Z29uZ0BnbWFpbC5jb20%3D; tid_7rurxl00clkk6pq1fwqc6j6yxh9mzs9t197jgtefvpplk7hy65=3.RM0.AzirZA.A-Ff.YL8N..uedn.b..s.eko.a.VT7bdQ.VT7ifQ.Cb9-gQ; mindfulness-daily-free-gift=1'
     cookies = {}
     for line in cookie_str.split(';'):
@@ -29,24 +30,30 @@ def get_meditation():
     headers = {'user-Agent': user_agent, 'host': host}
     html = requests.get(base_url, headers=headers, cookies=cookies)
     soup = BeautifulSoup(html.text, features="lxml")
+
+    # 分组
     subsubject = soup.find_all(attrs={"class":"sub-category-head"})
     subject_name = [x.text.strip() for x in subsubject]
     # print(subject_name)
 
+    # 组内的小节
     session_count = soup.find_all(attrs={"class":"session-buttons"})
     session_length = [len(x.find_all('li')) for x in session_count]
     # print(session_length)
 
+    # 每个独立的音频
     session_titles = soup.find_all(attrs={"class":"noselect"})
     session_title = [x['data-session-title'].strip().replace('+', '_') for x in session_titles[4:]]
     # print(session_title)
 
+    # 下载每个音频
     count = 1
     for i in range(len(subject_name)):
         subject_path = './meditation/' + str(i+1)+'_' + subject_name[i].replace(' ', '_') + '/'
         if not os.path.exists(subject_path):
             os.mkdir(subject_path)
         for j in range(session_length[i]):
+            # 这个url是分析视频地址的规律得到的
             url = 'http://soundstrue-ha.s3.amazonaws.com/oce/journeys/mindfulness_daily/audio/WC04753_' + str("%02d"%count) + '.mp3?AWSAccessKeyId=AKIAITC5TSWECGY4SOJQ'
             file_name = str(j+1) + '_' + session_title[count-1] + '.mp3'
             path = subject_path + '/' + file_name
@@ -59,6 +66,7 @@ def get_meditation():
                 print(count)
             count += 1
         print(subject_name[i], 'done')
+
 
 
 if __name__ == '__main__':
